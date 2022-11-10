66cbbb2514a4c6ea7134ad86851302e2329cdc44
kjh7592 <sajh0405@gmail.com>
2022-11-10 19:16:10 +0900

    JAM 시작

---
+++ src/com/KoreaIT/example/JAM/Main.java
@@ -0,0 +1,25 @@
+package com.KoreaIT.example.JAM;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("=== 프로그램 시작===");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			System.out.printf("명령어)");
+			String cmd = sc.nextLine().trim();
+			
+			if(cmd.equals("exit")) {
+				System.out.println("프로그램을 종료합니다");
+				break;
+			}
+		}
+		
+		sc.close();
+		
+		System.out.println("=== 프로그램 끝===");
+	}
+}
