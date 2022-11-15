9a8a2d9452c7440f5f3df17548881b4eba466303
kjh7592 <sajh0405@gmail.com>
2022-11-15 10:42:16 +0900

    게시물 상세보기 기능 구현

--- src/com/KoreaIT/example/JAM/App.java
+++ src/com/KoreaIT/example/JAM/App.java
@@ -80,6 +80,32 @@ public class App {
 			
 			System.out.printf("%d번 글이 생성 되었습니다\n", id);
 			
+		}else if(cmd.startsWith("article detail ")) {
+			int id = Integer.parseInt(cmd.split(" ")[2]);
+			
+			SecSql sql = new SecSql();
+			
+			sql.append("SELECT *");
+			sql.append("FROM article");
+			sql.append("WHERE id = ?", id);
+			
+			Map<String, Object> articleMap = DBUtil.selectRow(conn, sql);
+			
+			if(articleMap.isEmpty()) {
+				System.out.printf("%d번 게시글은 존재하지 않습니다\n", id);
+				return;
+			}
+			
+			System.out.printf("==%d번 게시물 상세보기 ==\n", id);
+			
+			Article article = new Article(articleMap);
+			
+			System.out.printf("번호 : %d\n", article.id);
+			System.out.printf("작성날짜 : %s\n", article.regDate);
+			System.out.printf("수정날짜 : %s\n", article.updateDate);
+			System.out.printf("제목 : %s\n", article.title);
+			System.out.printf("내용 : %s\n", article.body);
+			
 		}else if(cmd.startsWith("article modify ")) {
 			int id = Integer.parseInt(cmd.split(" ")[2]);
 			System.out.printf("==%d번 게시물 수정 ==\n", id);
@@ -90,13 +116,13 @@ public class App {
 			String body = sc.nextLine();
 			
 			SecSql sql = new SecSql();
-
+			
 			sql.append("UPDATE article");
 			sql.append("SET updateDate = NOW()");
 			sql.append(", title = ?", title);
 			sql.append(", `body` = ?", body);
 			sql.append("WHERE id = ?", id);
-
+			
 			DBUtil.update(conn, sql);
 			
 			System.out.printf("%d번 글이 수정 되었습니다\n", id);
